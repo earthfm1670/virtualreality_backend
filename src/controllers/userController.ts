3a7b9b601b46edda7b4252e54ec85e299b03f31a
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2025-02-21 08:30:02 +0700

    add: controller for user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,3 +1,25 @@
-import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/user";
 
+export async function getUsers() {
+  return await User.findAll();
+}
+
+export async function getUserById(userId: number) {
+  return await User.findByPk(userId);
+}
+
+export async function createUser(userData: any) {
+  return await User.create(userData);
+}
+
+export async function updateUser(userId: number, updatedData: any) {
+  const user = await User.findByPk(userId);
+  if (!user) throw new Error("User not found");
+  return await user.update(updatedData);
+}
+
+export async function deleteUser(userId: number) {
+  const user = await User.findByPk(userId);
+  if (!user) throw new Error("User not found");
+  return await user.destroy();
+}
